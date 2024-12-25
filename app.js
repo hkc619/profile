047e594d6ba4b2ad198c6e0e0b5e6508d9a083cb
hkc619 <kendrick10112001@gmail.com>
2024-12-26 01:09:03 +0800

    Html new idea. 1226, 2024

---
+++ app.js
@@ -0,0 +1,7 @@
+const schoolButton = document.getElementById("schoolButton");
+const workButton = document.getElementById("workButton");
+const toolButton = document.getElementById("toolButton");
+
+schoolButton.addEventListener("click", () => {});
+workButton.addEventListener("click", () => {});
+toolButton.addEventListener("click", () => {});

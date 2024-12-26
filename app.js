de72703736cfa398773ac9d0ec3cb3da13a7c338
hkc619 <kendrick10112001@gmail.com>
2024-12-26 20:24:52 +0800

    Update css. 1226, 2024

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const schoolButton = document.getElementById("schoolButton");
 const workButton = document.getElementById("workButton");
 const toolButton = document.getElementById("toolButton");
 
-schoolButton.addEventListener("click", () => {});
-workButton.addEventListener("click", () => {});
-toolButton.addEventListener("click", () => {});
+schoolButton.addEventListener("click", () => {
+  opacity = 0;
+});
+workButton.addEventListener("click", () => {
+  opacity = 0;
+});
+toolButton.addEventListener("click", () => {
+  opacity = 0;
+});

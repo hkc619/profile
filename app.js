175d8302aeb25d2f53b786e3722c0f4b969a3f58
hkc619 <kendrick10112001@gmail.com>
2024-12-28 20:59:10 +0800

    Adjust css. 1228, 2024

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const workButton = document.getElementById("workButton");
 const toolButton = document.getElementById("toolButton");
 
 schoolButton.addEventListener("click", () => {
-  opacity = 0;
+  //更改id或class 讓其他的變成 -notshow opacity=100  這個變成 -show opacity=0
 });
 workButton.addEventListener("click", () => {
   opacity = 0;
